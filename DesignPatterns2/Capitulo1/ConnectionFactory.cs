48a5fde8f31c1e56cbdcfea6767eaa0f5c3c202b
gilmarsouza <gilmar.limasou@gmail.com>
2017-12-27 21:52:41 -0200

    Add project files.

---
+++ DesignPatterns2/Capitulo1/ConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Capitulo1
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            var conexao = new SqlConnection();
+            conexao.ConnectionString = "User Id=root;Password=;Server=localhost;Database=banco";
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
---
+++ DesignPatterns2/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns2.Capitulo1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conexao = new ConnectionFactory().GetConnection();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = "select * from tabela";
+        }
+    }
+}
